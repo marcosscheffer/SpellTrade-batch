be0e3cf93c3dd49be6bd2d1ecaa4d272a4951a34
Marcos <marcosscheffer2989@gmail.com>
2026-06-20 14:31:00 -0300

    changed schedulet for 03:00AM everyday

--- src/main/java/com/marcos/cards_batch/config/JobScheduler.java
+++ src/main/java/com/marcos/cards_batch/config/JobScheduler.java
@@ -20,8 +20,8 @@ public class JobScheduler {
         this.importScryfallCardsJob = importScryfallCardsJob;
     }
 
-    //@Scheduled(cron = "0 0 3 * * *")
-    @Scheduled(fixedRate = 600000)
+    @Scheduled(cron = "0 0 3 * * *")
+    //@Scheduled(fixedRate = 600000)
     public void runImportJob() {
         try {
             JobParameters params = new JobParametersBuilder()
